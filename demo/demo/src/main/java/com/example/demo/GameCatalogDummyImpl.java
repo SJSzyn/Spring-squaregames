1e3e430e1389d06877cf837a695097270f975f7a
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-02-16 16:51:23 +0100

    Started kit 2

---
+++ demo/demo/src/main/java/com/example/demo/GameCatalogDummyImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import fr.le_campus_numerique.square_games.engine.tictactoe.TicTacToeGameFactory;
+
+public class GameCatalogDummyImpl implements GameCatalog{
+    
+
+}
