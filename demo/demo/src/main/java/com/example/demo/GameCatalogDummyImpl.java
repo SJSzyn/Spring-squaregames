ad9fedbc95f9afc06b5b1e385a9e0a42558b5611
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-02-19 09:29:30 +0100

    Finished part 1.1

--- demo/demo/src/main/java/com/example/demo/GameCatalogDummyImpl.java
+++ demo/demo/src/main/java/com/example/demo/GameCatalogDummyImpl.java
@@ -1,8 +1,23 @@
 package com.example.demo;
 
 import fr.le_campus_numerique.square_games.engine.tictactoe.TicTacToeGameFactory;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Component;
 
-public class GameCatalogDummyImpl implements GameCatalog{
-    
+@Component
+public class GameCatalogDummyImpl implements GameCatalog {
 
+    private TicTacToeGameFactory ticTacToeGameFactory;
+
+    public GameCatalogDummyImpl() {
+        this.ticTacToeGameFactory = new TicTacToeGameFactory();
+        // Any additional setup can be done here
+    }
+
+    @Override
+    public List<String> getGameIdentifiers() {
+        // Return a dummy list of identifiers or use the TicTacToeGameFactory instance as needed
+        return Arrays.asList("TicTacToe"); // Example identifiers
+    }
 }
--- demo/demo/src/main/java/com/example/demo/HeartbeatController.java
+++ demo/demo/src/main/java/com/example/demo/HeartbeatController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
 
 @RestController
 public class HeartbeatController {
@@ -10,8 +11,17 @@ public class HeartbeatController {
     @Autowired
     private HeartbeatSensor heartbeatSensor;
 
+    @Autowired
+    private GameCatalog gameCatalog;
+
     @GetMapping("/heartbeat")
     public int getHeartbeat() {
         return heartbeatSensor.get();
+    }
+
+    // Nouveau endpoint pour obtenir les identifiants des jeux
+    @GetMapping("/games/identifiers")
+    public Collection<String> getGameIdentifiers() {
+        return gameCatalog.getGameIdentifiers();
     }
 }
