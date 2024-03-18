2c784c011ae8a6a28243c9b50c1f84d07f7b29c8
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-03-18 16:26:57 +0100

    Added dockerfile

--- demo/demo/src/main/java/com/example/demo/squaregames/repository/UserRepositoryAuth.java
+++ demo/demo/src/main/java/com/example/demo/squaregames/repository/UserRepositoryAuth.java
@@ -1,13 +1,13 @@
-package com.example.demo.squaregames.repository;
-
-import com.example.demo.squaregames.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface UserRepositoryAuth extends CrudRepository<UserEntity, Integer> {
-
-    Optional<UserEntity> findByUsername(String username);
-}
+//package com.example.demo.squaregames.repository;
+//
+//import com.example.demo.squaregames.entity.UserEntity;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface UserRepositoryAuth extends CrudRepository<UserEntity, Integer> {
+//
+//    Optional<UserEntity> findByUsername(String username);
+//}
--- demo/demo/src/main/java/com/example/demo/squaregames/service/MyUserDetailsService.java
+++ demo/demo/src/main/java/com/example/demo/squaregames/service/MyUserDetailsService.java
@@ -1,23 +1,23 @@
-package com.example.demo.squaregames.service;
-
-import com.example.demo.squaregames.dao.MySQLUserDAOJDBC;
-import com.example.demo.squaregames.repository.UserRepositoryAuth;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserRepositoryAuth userRepositoryAuth;
-
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        MySQLUserDAOJDBC mySQLUserDAOJDBC = new MySQLUserDAOJDBC();
-        return null;
-    }
-}
+//package com.example.demo.squaregames.service;
+//
+//import com.example.demo.squaregames.dao.MySQLUserDAOJDBC;
+//import com.example.demo.squaregames.repository.UserRepositoryAuth;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MyUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserRepositoryAuth userRepositoryAuth;
+//
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        MySQLUserDAOJDBC mySQLUserDAOJDBC = new MySQLUserDAOJDBC();
+//        return null;
+//    }
+//}
